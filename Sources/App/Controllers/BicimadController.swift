5e4c3562319e4bfc81662fdbc267553e488dd326
Jorge Benjumea <me@intron014.com>
2024-09-25 17:17:45 +0200

    BiciMAD Branding for this controller

--- Sources/App/Controllers/BicimadController.swift
+++ Sources/App/Controllers/BicimadController.swift
@@ -1,32 +1,53 @@
 import Vapor
 import Foundation
 
-struct EMTStations: Content {
+/// getBicimadFavs
+struct bicimadStations: Content {
     var name: String
 }
-struct getEMTFavsResponse: Content {
-    var data: [EMTStations]
+struct getBicimadFavsResponse: Content {
+    var data: [bicimadStations]
+}
+
+///getBicimadRel
+struct BicimadRel: Content {
+    var id: Int
+    var bid: Int
+}
+struct getBicimadRelResponse: Content {
+    var data: [BicimadRel]
 }
 
 struct BicimadController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
         let bicimad = routes.grouped("bicimad")
         
-        bicimad.get("emt-stars", use: getEMTFavs)
+        bicimad.get("stars", use: getBicimadFavs)
+        bicimad.get("rel", use: getBicimadRel)
     }
     
     @Sendable
-    func getEMTFavs(req: Request) throws -> getEMTFavsResponse {
+    func getBicimadFavs(req: Request) throws -> getBicimadFavsResponse {
         let stations = [
-            EMTStations(name: "451 - Home"),
-            EMTStations(name: "436 - UPM"),
-            EMTStations(name: "437 - Sierra de Guadalupe"),
-            EMTStations(name: "440 - Villa de Vallecas"),
-            EMTStations(name: "000 - Other")
+            bicimadStations(name: "451 - Home"),
+            bicimadStations(name: "436 - UPM"),
+            bicimadStations(name: "437 - Sierra de Guadalupe"),
+            bicimadStations(name: "440 - Villa de Vallecas"),
+            bicimadStations(name: "000 - Other")
         ]
-        return getEMTFavsResponse(data: stations)
+        return getBicimadFavsResponse(data: stations)
     }
     
+    @Sendable
+    func getBicimadRel(req: Request) throws -> getBicimadRelResponse {
+        let bikerel = [
+            BicimadRel(id: 2078, bid: 436),
+            BicimadRel(id: 2003, bid: 437),
+            BicimadRel(id: 2053, bid: 440),
+            BicimadRel(id: 2188, bid: 451)
+        ]
+        return getBicimadRelResponse(data: bikerel)
+    }
     
     
 }
