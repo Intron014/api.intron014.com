5b423504beb7cc91ff2ec4556152371d0659696a
Jorge Benjumea <me@intron014.com>
2024-10-04 21:23:46 +0200

    We now return a JSON instead of a straight up string

--- Sources/App/Controllers/ClipboardController.swift
+++ Sources/App/Controllers/ClipboardController.swift
@@ -10,14 +10,18 @@ struct ClipboardController: RouteCollection {
     }
     
     @Sendable
-    func modifyClipboardLink(req: Request) async throws -> LinkRequest {
+    func modifyClipboardLink(req: Request) async throws -> Response {
         do {
             let linkData = try req.content.decode(LinkRequest.self)
             let modifiedLink = linkData.link.replacingOccurrences(of: "?forcedownload=1", with: "?forcedownload=0")
             
             req.logger.info("mod: \(modifiedLink)")
             
-            return LinkRequest(link: modifiedLink)
+            let jsonResponse = Response(status: .ok)
+            try jsonResponse.content.encode(LinkRequest(link: modifiedLink))
+            jsonResponse.headers.contentType = .json
+            
+            return jsonResponse
         } catch {
             req.logger.error("Error processing request: \(error)")
             throw Abort(.internalServerError)
