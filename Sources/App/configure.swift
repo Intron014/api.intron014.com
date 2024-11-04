883e163b0677b62be6c4c087c09fb3aaf0e1578d
Jorge Benjumea <me@intron014.com>
2024-11-04 13:24:43 +0100

    chore: CORS

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -20,5 +20,13 @@ public func configure(_ app: Application) async throws {
 
     //app.views.use(.leaf)
 
+    let corsConfiguration = CORSMiddleware.Configuration(
+        allowedOrigin: .all,
+        allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
+        allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith]
+    )
+    let cors = CORSMiddleware(configuration: corsConfiguration)
+
+    app.middleware.use(cors)
     try routes(app)
 }
